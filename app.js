0b0f9a962930ab6e105e73bf1344624db63275fb
Divyanshu Singh <singhdivyanshu2707@gmail.com>
2024-03-27 17:02:42 +0530

    feat: connected fe to be

--- app.js
+++ app.js
@@ -4,21 +4,24 @@ const mongoose = require("mongoose");
 const router = require("./routes/routes");
 const port = 3000;
 
+const cors = require("cors");
+
 const app = express();
+app.use(cors());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 mongoose
-  .connect(process.env.DATABASE + "/" + process.env.DATABASE_NAME)
-  .then(() => {
-    console.log("Database connected");
-    app.listen(port, () => {
-      console.log("Listening to port ", port);
-    });
-  })
-  .catch((error) => {
-    console.log("Database connection failed");
-    console.error(error);
-  });
+	.connect(process.env.DATABASE + "/" + process.env.DATABASE_NAME)
+	.then(() => {
+		console.log("Database connected");
+		app.listen(port, () => {
+			console.log("Listening to port ", port);
+		});
+	})
+	.catch((error) => {
+		console.log("Database connection failed");
+		console.error(error);
+	});
 
 app.use("/", router);
